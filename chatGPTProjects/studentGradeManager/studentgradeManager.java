c2c5a72fe766fcec27f7f3b8613b1eaec9022b38
lokin7778 <lokin7778@gmail.com>
2025-11-01 17:52:48 -0400

    imported all of the modules required for the project and declared the package

---
+++ chatGPTProjects/studentGradeManager/studentgradeManager.java
@@ -0,0 +1,7 @@
+package chatGPTProjects.studentGradeManager; // directs it to the package
+
+// imported all the necessary modules required for this project
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
