d8362b582a90b1dcab65e9ae5d26fe46fef01203
lokin7778 <lokin7778@gmail.com>
2025-11-01 17:59:54 -0400

    Added the basic skeletal code and then created a while loop to prompt the user the student's name (till now)

--- chatGPTProjects/studentGradeManager/studentgradeManager.java
+++ chatGPTProjects/studentGradeManager/studentgradeManager.java
@@ -5,3 +5,21 @@ import java.util.Scanner;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+
+public class studentgradeManager{
+
+    public static void main(String[] args) {
+        
+        // creates a scanner object 'console'
+        Scanner console = new Scanner(System.in);
+
+        String optionToContinue = "Y";
+
+        while(optionToContinue.equals("Y")){
+
+            System.out.print("Enter the student name: ");
+            String sName = console.nextLine();
+        }
+
+    }
+}
